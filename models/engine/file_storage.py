b83581cf329f6bfb5c086ccc6ddb485b3f858fcc
santaETH <sdamissie@yahoo.com>
2024-03-24 10:43:09 +0300

    AirBnB clone MySql

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This is the file storage class for AirBnB"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 import shlex
+
 
 class FileStorage:
     """This class serializes instances to a JSON file and

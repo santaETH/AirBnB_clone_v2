41b2969bb26f36cd6a2547e8870eb3daf1ec330f
santaETH <sdamissie@yahoo.com>
2024-05-19 10:48:26 -0700

    deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Write a Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Write a fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers.
 
 execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/school -u ubuntu
 """
